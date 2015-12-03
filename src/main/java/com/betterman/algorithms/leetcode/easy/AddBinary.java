a96bb3623d5203240fab1c31a5142b54723751c5
Zhuangda Zhu <zhuangdaz@gmail.com>
2015-12-02 23:29:14 -0800

    Simplify LC#67

--- src/main/java/com/betterman/algorithms/leetcode/easy/AddBinary.java
+++ src/main/java/com/betterman/algorithms/leetcode/easy/AddBinary.java
@@ -15,21 +15,9 @@ public class AddBinary {
                 int y = i < lenB && b.charAt(lenB - 1 - i) == '1' ? 1 : 0;
 
                 int sum = x + y + carry;
-                if (sum == 0) {
-                    sb.insert(0, "0");
-                    carry = 0;
-                } else if (sum == 1) {
-                    sb.insert(0, "1");
-                    carry = 0;
-                } else if (sum == 2) {
-                    sb.insert(0, "0");
-                    carry = 1;
-                } else {
-                    sb.insert(0, "1");
-                    carry = 1;
-                }
+                carry = sum / 2;
+                sb.insert(0, sum % 2);
             }
-            if (sb.length() > 1 && sb.charAt(0) == '0') sb.deleteCharAt(0);
             return sb.toString();
         }
     }
