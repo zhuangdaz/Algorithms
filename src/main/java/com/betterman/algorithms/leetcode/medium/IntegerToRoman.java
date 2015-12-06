1fb1fe7cda95287346f7e7549f5efcdaa3119e89
Zhuangda Zhu <zhuangdaz@gmail.com>
2015-12-06 10:10:07 -0800

    [#1]Integer to roman

---
+++ src/main/java/com/betterman/algorithms/leetcode/medium/IntegerToRoman.java
@@ -0,0 +1,47 @@
+package com.betterman.algorithms.leetcode.medium;
+
+/**
+ * Created by zhuangda on 12/6/15.
+ */
+public class IntegerToRoman {
+    public class Solution {
+        public String intToRoman(int num) {
+            if (num > 3999 || num < 1) return null;
+            char[][] table = new char[][]{
+                {'I', 'V', 'X'},
+                {'X', 'L', 'C'},
+                {'C', 'D', 'M'},
+                {'M', '*', '#'}
+            };
+
+            int i = 0;
+            StringBuffer ret = new StringBuffer();
+            while (num != 0) {
+                int d = num % 10;
+
+                if (d > 0) {
+                    StringBuffer sec = new StringBuffer();
+                    if (d == 4) {
+                        sec.append(table[i][0]).append(table[i][1]);
+                    } else if (d == 9) {
+                        sec.append(table[i][0]).append(table[i][2]);
+                    } else {
+                        if (d / 5 == 1) {
+                            sec.append(table[i][1]);
+                            d %= 5;
+                        }
+                        while (d-- > 0) {
+                            sec.append(table[i][0]);
+                        }
+                    }
+                    ret.insert(0, sec.toString());
+                }
+
+                num /= 10;
+                i++;
+            }
+
+            return ret.toString();
+        }
+    }
+}
