bd84962b53d875532271891d2349db35e91b24e3
Zhuangda Zhu <zhuangdaz@gmail.com>
2015-12-10 22:33:35 -0800

    [#2]LongestSubStrTwoDistinct BEAT 100%git add -u

---
+++ src/main/java/com/betterman/algorithms/leetcode/hard/LongestSubStrTwoDistinct.java
@@ -0,0 +1,41 @@
+package com.betterman.algorithms.leetcode.hard;
+
+/**
+ * Created by zhuangda on 12/10/15.
+ */
+public class LongestSubStrTwoDistinct {
+    public class Solution {
+        public int lengthOfLongestSubstringTwoDistinct(String s) {
+            if (s.isEmpty()) return 0;
+            int max = 1;
+            int p1 = 0, p2 = 0;
+            int last = 1;
+            char[] chars = s.toCharArray();
+            for (int i = 1; i < chars.length; i++) {
+                if (p1 != p2 && chars[i] != chars[p1] && chars[i] != chars[p2]) {
+                    if (last > max) max = last;
+
+                    last = i - p1;
+                    p1 = p2;
+                    p2 = i;
+                } else if (p1 == p2) {
+                    if (chars[i] == chars[p1]) {
+                        p1 = i;
+                        p2 = i;
+                    }
+                    last++;
+                    p2 = i;
+                } else {
+                    if (chars[i] == chars[p1]) {
+                        p1 = p2;
+                    }
+                    last++;
+                    p2 = i;
+                }
+            }
+
+            if (last > max) max = last;
+            return max;
+        }
+    }
+}
