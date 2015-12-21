5e6fce3796a472444bdf0a5e9df97e047e1c88a6
Zhuangda Zhu <zhuangdaz@gmail.com>
2015-12-21 00:22:35 -0800

    Neat code

--- src/main/java/com/betterman/algorithms/leetcode/easy/RemoveElement.java
+++ src/main/java/com/betterman/algorithms/leetcode/easy/RemoveElement.java
@@ -17,5 +17,13 @@ public class RemoveElement {
             }
             return nums.length - count;
         }
+
+        public int removeElementNeat(int[] nums, int val) {
+            int begin = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != val) nums[begin++] = nums[i];
+            }
+            return begin;
+        }
     }
 }
