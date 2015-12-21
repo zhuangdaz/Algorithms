1eb4064212e4162e8a65004c9ba60943c4a7a887
Zhuangda Zhu <zhuangdaz@gmail.com>
2015-12-21 00:08:28 -0800

    [#3]rotate2

--- src/main/java/com/betterman/algorithms/leetcode/easy/RoateArray.java
+++ src/main/java/com/betterman/algorithms/leetcode/easy/RoateArray.java
@@ -24,5 +24,25 @@ public class RoateArray {
                 nums[i] = tmpK[i];
             }
         }
+
+        public void rotate2(int[] nums, int k) {
+            if (k % nums.length == 0) return;
+
+            int n = nums.length;
+            k %= n;
+            reverse(nums, 0, n - 1);
+            reverse(nums, 0, k - 1);
+            reverse(nums, k, n - 1);
+        }
+
+        private void reverse(int[] nums, int start, int end) {
+            while (start < end) {
+                int tmp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = tmp;
+                start++;
+                end--;
+            }
+        }
     }
 }
