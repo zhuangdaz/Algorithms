9671e99a43014d6e3721c8480e3389e6b5297008
Zhuangda Zhu <zhuangdaz@gmail.com>
2015-12-21 23:39:25 -0800

    [#3] optimize

--- src/main/java/com/betterman/algorithms/leetcode/easy/ContainsDuplicateII.java
+++ src/main/java/com/betterman/algorithms/leetcode/easy/ContainsDuplicateII.java
@@ -1,7 +1,9 @@
 package com.betterman.algorithms.leetcode.easy;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by zhuangda on 12/21/15.
@@ -15,6 +17,19 @@ public class ContainsDuplicateII {
 
                 if (map.containsKey(num) && i - map.get(num) <= k) return true;
                 else map.put(num, i);
+            }
+            return false;
+        }
+
+        //Use set instead of map
+        // Remove number from set once it's out of range which is k
+        public boolean containsNearbyDuplicateWithSet(int[] nums, int k) {
+            Set<Integer> set = new HashSet();
+            for (int i = 0; i < nums.length; i++) {
+                if (i > k) {
+                    set.remove(nums[i - k - 1]);
+                }
+                if (!set.add(nums[i])) return true;
             }
             return false;
         }
