6e31fd630d5f04706dc949c50491ce2f166dbc2d
Zhuangda Zhu <zhuangdaz@gmail.com>
2015-12-24 12:30:29 -0800

    [#4]3 sum

--- src/main/java/com/betterman/algorithms/leetcode/medium/ThreeSum.java
+++ src/main/java/com/betterman/algorithms/leetcode/medium/ThreeSum.java
@@ -2,7 +2,6 @@ package com.betterman.algorithms.leetcode.medium;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -10,49 +9,82 @@ import java.util.List;
  */
 public class ThreeSum {
 
-    public class Solution {
+    public static class Solution {
         // sort array
         // one pointer to traverse the array
         // two more pointers: one points to i+1, another one points to the end
         public List<List<Integer>> threeSum(int[] nums) {
-            List<List<Integer>> ret = new ArrayList();
+            Arrays.sort(nums);
 
-            Arrays.sort(nums); //O(nlogn)
+            List<List<Integer>> res = new ArrayList();
 
-            for (int i = 0; i < nums.length - 2; i++) {
-                if (i > 0 && nums[i] == nums[i - 1]) continue;
-                if (nums[i] > 0) break; //already greater than 0
+            int i = 0;
+            while (i < nums.length - 2) {
+                int l = i + 1;
+                int r = nums.length - 1;
+                int sum = nums[i] * -1;
 
-                int j = i + 1;
-                int k = nums.length - 1;
-                while (j < k) {
-                    if (j > i + 1 && nums[j] == nums[j - 1]) {
-                        j++;
-                        continue;
-                    }
+                while (l < r) {
+                    int s = nums[l] + nums[r];
+                    if (s < sum) l++;
+                    else if (s > sum) r--;
+                    else {
+                        List<Integer> match = new ArrayList();
+                        match.add(nums[i]);
+                        match.add(nums[l]);
+                        match.add(nums[r]);
+                        res.add(match);
+                        l++;
+                        r--;
 
-                    if (nums[i] + nums[j] > 0) break;
-
-                    int sum = nums[j] + nums[k];
-
-                    if (sum + nums[i] == 0) {
-                        List<Integer> triplet = new ArrayList();
-                        triplet.add(nums[i]);
-                        triplet.add(nums[j]);
-                        triplet.add(nums[k]);
-                        ret.add(triplet);
-
-                        j++;
-                        k--;
-                    } else if (sum + nums[i] < 0) {
-                        j++;
-                    } else {
-                        k--;
+                        while (l < nums.length - 1 && nums[l] == nums[l - 1]) l++;
+                        while (r > 0 && nums[r] == nums[r + 1]) r--;
                     }
                 }
 
+                i++;
+                while (i < nums.length - 2 && nums[i] == nums[i - 1]) i++;
             }
-            return ret;
+            return res;
         }
+
+        public List<List<Integer>> threeSumConcise(int[] nums) {
+            Arrays.sort(nums);
+
+            List<List<Integer>> res = new ArrayList();
+
+            //if i must be incremented for each iteration then use FOR loop.
+            //otherwise, use WHILE loop.
+            for (int i = 0; i < nums.length - 2; i++) {
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+                if (nums[i] > 0) break;
+
+                int target = nums[i] * -1;
+                int lo = i + 1;
+                int hi = nums.length - 1;
+
+                while (lo < hi) {
+                    int sum = nums[lo] + nums[hi];
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                        while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
+                        while (lo < hi && nums[hi] == nums[hi - 1]) hi--;
+
+                        lo++;
+                        hi--;
+                    }
+                    else if (sum < target) lo++;
+                    else hi--;
+                }
+            }
+            return res;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[]{-1, 0, 1};
+        System.out.println(solution.threeSum(nums));
     }
 }
