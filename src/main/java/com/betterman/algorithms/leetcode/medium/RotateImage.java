c2871ca4d60d1962928f44a8b5760c81c7b21ace
Zhuangda Zhu <zhuangdaz@gmail.com>
2015-12-25 09:31:48 -0800

    [#4]rotate image opt

--- src/main/java/com/betterman/algorithms/leetcode/medium/RotateImage.java
+++ src/main/java/com/betterman/algorithms/leetcode/medium/RotateImage.java
@@ -26,5 +26,42 @@ public class RotateImage {
                 }
             }
         }
+
+        /**
+         *
+         * clockwise rotate
+         * first reverse up to down, then swap the symmetry
+         * 1 2 3     7 8 9     7 4 1
+         * 4 5 6  => 4 5 6  => 8 5 2
+         * 7 8 9     1 2 3     9 6 3
+         */
+
+        /**
+         * anticlockwise rotate
+         * first reverse left to right, then swap the symmetry
+         * 1 2 3     3 2 1     3 6 9
+         * 4 5 6  => 6 5 4  => 2 5 8
+         * 7 8 9     9 8 7     1 4 7
+         */
+        public void rotate2(int[][] matrix) {
+            int n = matrix.length;
+
+            for (int i = 0; i < n / 2; i++) {
+                for (int j = 0; j < n; j++)
+                    swap(matrix, i, j, n - 1 - i, j);
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+                    swap(matrix, i, j, j, i);
+                }
+            }
+        }
+
+        private void swap (int[][] matrix, int x1, int y1, int x2, int y2) {
+            int tmp = matrix[x1][y1];
+            matrix[x1][y1] = matrix[x2][y2];
+            matrix[x2][y2] = tmp;
+        }
     }
 }
