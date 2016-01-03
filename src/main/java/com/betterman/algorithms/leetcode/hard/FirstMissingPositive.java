8be598d4b909d6d7200c4087d871bf0586a68607
Zhuangda Zhu <zhuangdaz@gmail.com>
2016-01-02 19:54:53 -0800

    [#5]first missing positive

---
+++ src/main/java/com/betterman/algorithms/leetcode/hard/FirstMissingPositive.java
@@ -0,0 +1,24 @@
+package com.betterman.algorithms.leetcode.hard;
+
+/**
+ * Created by zhuangda on 1/2/16.
+ */
+public class FirstMissingPositive {
+    public class Solution {
+        public int firstMissingPositive(int[] nums) {
+            for (int i = 0; i < nums.length; i++) {
+                while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+                    int tmp = nums[nums[i] - 1];
+                    nums[nums[i] - 1] = nums[i];
+                    nums[i] = tmp;
+                }
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != i + 1) return i + 1;
+            }
+
+            return nums.length + 1;
+        }
+    }
+}
