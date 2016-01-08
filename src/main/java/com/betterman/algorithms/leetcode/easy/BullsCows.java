58d82cce19a1d8ae860cf26ae7f4dd059448f12c
Zhuangda Zhu <zhuangdaz@gmail.com>
2016-01-08 00:12:52 -0800

    [#6]Bulls and Cows

---
+++ src/main/java/com/betterman/algorithms/leetcode/easy/BullsCows.java
@@ -0,0 +1,57 @@
+package com.betterman.algorithms.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zhuangda on 1/8/16.
+ */
+
+/**
+ * You are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
+ * <p>
+ * For example:
+ * <p>
+ * Secret number:  "1807"
+ * Friend's guess: "7810"
+ * Hint: 1 bull and 3 cows. (The bull is 8, the cows are 0, 1 and 7.)
+ * Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows. In the above example, your function should return "1A3B".
+ * <p>
+ * Please note that both secret number and friend's guess may contain duplicate digits, for example:
+ * <p>
+ * Secret number:  "1123"
+ * Friend's guess: "0111"
+ * In this case, the 1st 1 in friend's guess is a bull, the 2nd or 3rd 1 is a cow, and your function should return "1A1B".
+ * You may assume that the secret number and your friend's guess only contain digits, and their lengths are always equal.
+ */
+public class BullsCows {
+    public class Solution {
+        //"1122 1222"
+        public String getHint(String secret, String guess) {
+            int bulls = 0, cows = 0;
+            int[] counts = new int[10];
+            List<Character> list = new ArrayList();
+            char[] s = secret.toCharArray();
+            char[] g = guess.toCharArray();
+
+            for (int i = 0; i < s.length; i++) {
+                if (s[i] != g[i]) {
+                    list.add(g[i]);
+                    counts[s[i] - '0']++;
+                } else {
+                    bulls++;
+                }
+            }
+
+            for (int i = 0; i < list.size(); i++) {
+                char c = list.get(i);
+                if (counts[c - '0'] != 0) {
+                    cows++;
+                    counts[c - '0']--;
+                }
+            }
+
+            return bulls + "A" + cows + "B";
+        }
+    }
+}
