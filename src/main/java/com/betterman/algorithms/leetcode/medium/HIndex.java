dccb2f523062f3e1dc2672cf18e0cce18d7c33d3
Zhuangda Zhu <zhuangdaz@gmail.com>
2016-01-11 21:58:43 -0800

    [#7]H index

--- src/main/java/com/betterman/algorithms/leetcode/medium/HIndex.java
+++ src/main/java/com/betterman/algorithms/leetcode/medium/HIndex.java
@@ -7,10 +7,28 @@ import java.util.Arrays;
  */
 public class HIndex {
     public class Solution {
-        public int hIndex(int[] citations) {
+        //O(nlgn)
+        public int hIndexSort(int[] citations) {
             Arrays.sort(citations);
             int h = 0, n = citations.length;
             while (h + 1 <= n && citations[n - (h + 1)] >= h + 1) h++;
+            return h;
+        }
+
+        //O(n)
+        public int hIndex(int[] citations) {
+            int n = citations.length;
+            int[] m = new int[n + 1];
+            for (int c : citations) {
+                if (c > n) m[n]++;
+                else m[c]++;
+            }
+
+            int h = n, count = m[n];
+            while (h >= 0 && h > count) {
+                h--;
+                count += m[h];
+            }
             return h;
         }
     }
