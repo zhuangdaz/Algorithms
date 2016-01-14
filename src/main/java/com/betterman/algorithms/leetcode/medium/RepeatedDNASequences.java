699eeda1256701c5b985e5e9388ad06314cdc9d3
Zhuangda Zhu <zhuangdaz@gmail.com>
2016-01-13 23:52:59 -0800

    [#7]repeated dna sequences

--- src/main/java/com/betterman/algorithms/leetcode/medium/RepeatedDNASequences.java
+++ src/main/java/com/betterman/algorithms/leetcode/medium/RepeatedDNASequences.java
@@ -9,7 +9,7 @@ import java.util.Set;
  * Created by zhuangda on 1/13/16.
  */
 public class RepeatedDNASequences {
-    public class Solution {
+    public static class Solution {
         public List<String> findRepeatedDnaSequences(String s) {
             List<String> res = new ArrayList();
             Set<String> words = new HashSet();
@@ -17,6 +17,29 @@ public class RepeatedDNASequences {
             for (int i = 9; i < s.length(); i++) {
                 String word = s.substring(i - 9, i + 1);
                 if (!words.add(word) && doubleWords.add(word)) res.add(word);
+            }
+            return res;
+        }
+
+        public List<String> findRepeatedDnaSequencesBit(String s) {
+            List<String> res = new ArrayList();
+            if (s.length() <= 10) return res;
+            Set<Integer> once = new HashSet();
+            Set<Integer> twice = new HashSet();
+            char[] chs = s.toCharArray();
+            int[] map = new int[26];
+            // map['A' - 'A'] = 0;
+            map['C' - 'A'] = 1;
+            map['G' - 'A'] = 2;
+            map['T' - 'A'] = 3;
+            int hash = 0;
+            for (int i = 0; i < 9; i++) {
+                hash = (hash << 2) | map[chs[i] - 'A'];
+            }
+
+            for (int i = 9; i < chs.length; i++) {
+                hash = (hash << 2) & 0xfffff | map[chs[i] - 'A'];
+                if (!once.add(hash) && twice.add(hash)) res.add(s.substring(i - 9, i + 1));
             }
             return res;
         }
