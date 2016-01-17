f2a332dc6e5bf86cb1c7f63cb6dd980b1631bcd6
Zhuangda Zhu <zhuangdaz@gmail.com>
2016-01-16 23:52:39 -0800

    [#9]reverse linked list

---
+++ src/main/java/com/betterman/algorithms/leetcode/common/ListNode.java
@@ -0,0 +1,13 @@
+package com.betterman.algorithms.leetcode.common;
+
+/**
+ * Created by zhuangda on 1/16/16.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/com/betterman/algorithms/leetcode/easy/ReverseLinkedList.java
@@ -0,0 +1,22 @@
+package com.betterman.algorithms.leetcode.easy;
+
+import com.betterman.algorithms.leetcode.common.ListNode;
+
+/**
+ * Created by zhuangda on 1/16/16.
+ */
+public class ReverseLinkedList {
+    public class Solution {
+        public ListNode reverseList(ListNode head) {
+            ListNode n = head;
+            ListNode prev = null;
+            while(n != null) {
+                ListNode next = n.next;
+                n.next = prev;
+                prev = n;
+                n = next;
+            }
+            return prev;
+        }
+    }
+}
