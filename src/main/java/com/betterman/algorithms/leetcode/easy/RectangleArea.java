a4601cc20e25857342e479e6071ac4e3efff0309
Zhuangda Zhu <zhuangdaz@gmail.com>
2016-01-22 00:05:39 -0800

    [#13]rectangle area

---
+++ src/main/java/com/betterman/algorithms/leetcode/easy/RectangleArea.java
@@ -0,0 +1,14 @@
+package com.betterman.algorithms.leetcode.easy;
+
+/**
+ * Created by zhuangda on 1/22/16.
+ */
+public class RectangleArea {
+    public class Solution {
+        public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+            int left = Math.max(A, E), right = Math.max(Math.min(C, G), left);
+            int bottom = Math.max(B, F), top = Math.max(Math.min(D, H), bottom);
+            return (C - A) * (D - B) - (right - left) * (top - bottom) + (G - E) * (H - F);
+        }
+    }
+}
