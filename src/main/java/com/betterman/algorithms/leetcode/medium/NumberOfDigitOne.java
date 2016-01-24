0e3aab51b986ee1e251b5e22d2476289d1db4170
Zhuangda Zhu <zhuangdaz@gmail.com>
2016-01-23 23:44:13 -0800

    [#13]number of digit one

---
+++ src/main/java/com/betterman/algorithms/leetcode/medium/NumberOfDigitOne.java
@@ -0,0 +1,27 @@
+package com.betterman.algorithms.leetcode.medium;
+
+/**
+ * Created by zhuangda on 1/23/16.
+ */
+public class NumberOfDigitOne {
+    public static class Solution {
+        public int countDigitOne(int n) {
+            int cnt = 0;
+
+            //use long to avoid overflow
+            for (long m = 1; m <= n; m *= 10) {
+                long a = n / m;
+                long b = n % m;
+                cnt += (a + 8) / 10 * m + (a % 10 == 1 ? b + 1 : 0);
+            }
+            return cnt;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+//        System.out.println(1000000000 * 10);  // ==1410065408 !?
+        System.out.println(solution.countDigitOne(1410065408));
+    }
+}
