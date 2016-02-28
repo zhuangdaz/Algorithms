06b65603f7a61ea45d23718c5316788f7eca926d
Zhuangda Zhu <zhuangdaz@gmail.com>
2016-02-28 15:47:42 -0800

    Backtracking medium

---
+++ src/main/java/com/betterman/algorithms/leetcode/medium/FlipGameII.java
@@ -0,0 +1,32 @@
+package com.betterman.algorithms.leetcode.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by zhuangda on 2/28/16.
+ */
+public class FlipGameII {
+    public class Solution {
+        public boolean canWin(String s) {
+            return canWin(s, new HashMap());
+        }
+
+        private boolean canWin(String s, Map<String, Boolean> mem) {
+            if (mem.containsKey(s)) return mem.get(s);
+
+            for (int i = 0; i < s.length() - 1; i++) {
+                if (s.charAt(i) != '+' || s.charAt(i + 1) != '+') continue;
+
+                String next = s.substring(0, i) + "--" + s.substring(i + 2);
+                if (!canWin(next, mem)) {
+                    mem.put(s, true);
+                    return true;
+                }
+            }
+            mem.put(s, false);
+            return false;
+        }
+
+    }
+}
