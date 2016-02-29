2082a998c3c0f9419485f9f8b3f6d1d77f531e82
Zhuangda Zhu <zhuangdaz@gmail.com>
2016-02-28 17:34:01 -0800

    Backtracking medium

---
+++ src/main/java/com/betterman/algorithms/leetcode/medium/GneralizedAbbreviation.java
@@ -0,0 +1,30 @@
+package com.betterman.algorithms.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zhuangda on 2/28/16.
+ */
+public class GneralizedAbbreviation {
+    public class Solution {
+        public List<String> generateAbbreviations(String word) {
+            List<String> ans = new ArrayList();
+            helper(word, 0, 0, new StringBuilder(), ans);
+            return ans;
+        }
+
+        private void helper(String word, int index, int count, StringBuilder path, List<String> ans) {
+            int len = path.length();
+            if (index >= word.length()) {
+                if (count > 0) path.append(count);
+                ans.add(path.toString());
+            } else {
+                helper(word, index + 1, count + 1, path, ans);
+                if (count > 0) path.append(count);
+                helper(word, index + 1, 0, path.append(word.charAt(index)), ans);
+            }
+            path.setLength(len);
+        }
+    }
+}
